ea9f1cec43c3d1e2016c64730e81455f6f6e6bfb
grikos <grikos@gmail.com>
2023-02-18 23:32:59 +0300

    Добавлен метод для получения информации по поддерживаемым полям таксономии событий

--- mpsiemlib/modules/EventsAPI.py
+++ mpsiemlib/modules/EventsAPI.py
@@ -10,7 +10,9 @@ class EventsAPI(ModuleInterface, LoggingHandler):
     """
     Модуль получения информации о событиях через API UI
     """
+    __api_events_metadata = "/api/events/v2/events_metadata"
     __api_event_details = "/api/events/v2/events/{}/normalized?time={}"
+    
 
     def __init__(self, auth: MPSIEMAuth, settings: Settings):
         ModuleInterface.__init__(self, auth, settings)
@@ -20,7 +22,8 @@ class EventsAPI(ModuleInterface, LoggingHandler):
         self.log.debug('status=succes, action=prepare, msg="EventsUI Module init"')
         
     def get_event_details(self, event_id, event_date) -> dict:
-        """Получить событие (все заполненные поля) по его идентификатору и дате
+        """
+        Получить событие (все заполненные поля) по его идентификатору и дате
 
         Args:
             event_id : идентификатор события
@@ -39,3 +42,19 @@ class EventsAPI(ModuleInterface, LoggingHandler):
                            'hostname="{}"'.format(self.__core_hostname))
             raise Exception("Core data request return None or has wrong response structure")
         return response.get("event")
+    
+    def get_events_metadata(self):
+        """
+        Получить список поддерживаемых полей таксономии событий
+        """
+        url = "https://{}{}".format(self.__core_hostname, self.__api_events_metadata)
+        rq = exec_request(self.__core_session, url)
+        response = rq.json()
+
+        if response is None or "fields" not in response:
+            self.log.error('status=failed, action=get_events_metadata, msg="Core data request return None or '
+                           'has wrong response structure", '
+                           'hostname="{}"'.format(self.__core_hostname))
+            raise Exception("Core data request return None or has wrong response structure")
+        return response.get("fields")    
+        
