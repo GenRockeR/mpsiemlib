39db30a771d9bab2ee49f29144b5e397b094483e
ochekhov <ochekhov@ptsecurity.com>
2024-02-26 07:47:56 +0000

    Filters export and import

--- mpsiemlib/modules/Filters.py
+++ mpsiemlib/modules/Filters.py
@@ -10,6 +10,11 @@ class Filters(ModuleInterface, LoggingHandler):
     __api_filters_list = "/api/v2/events/filters_hierarchy"
     __api_filter_info = "/api/v2/events/filters/{}"
 
+    #__api_filters = "/api/v3/events/filters/{}" # get filter info
+
+    __api_filter = "/api/v3/events/filters"
+    __api_folder = "/api/v2/events/folders"
+
     def __init__(self, auth: MPSIEMAuth, settings: Settings):
         ModuleInterface.__init__(self, auth, settings)
         LoggingHandler.__init__(self)
@@ -42,6 +47,49 @@ class Filters(ModuleInterface, LoggingHandler):
                       'hostname="{}"'.format(len(self.__folders), self.__core_hostname))
 
         return self.__folders
+
+    def create_event_filter_folder(self, folder_name: str, parent_id: str) -> str:
+        """
+        Создать директорию для фильтров
+        :param folder_name: имя создаваемой директории
+        :param parent_id: ID родительской директории
+
+        :return: folder_id: ID созданнной директории
+        """      
+
+        api_url = self.__api_folder
+        url = "https://{}{}".format(self.__core_hostname, api_url)
+        params = {'name': folder_name, 'parentId': parent_id}
+        r = exec_request(self.__core_session,
+                         url,
+                         method='POST',
+                         timeout=self.settings.connection_timeout,
+                         json=params)
+        r = r.json()
+        folder_id = r.get("folderId")
+        return folder_id
+
+    def create_event_filter(self, filter_name: str, folder_id: str, pdql_query: str) -> str:
+        """
+        Создать директорию для фильтров
+        :param filter_name: имя создаваемого фильтра
+        :param folder_id: ID директории, в которой создается фильтр
+        :param pdql_query: поисковой запрос
+
+        :return: filter_id: ID созданнного фильтра
+        """      
+
+        api_url = self.__api_filter
+        url = "https://{}{}".format(self.__core_hostname, api_url)
+        params = {'folderId':folder_id, 'name': filter_name, 'pdqlQuery': pdql_query}
+        r = exec_request(self.__core_session,
+                         url,
+                         method='POST',
+                         timeout=self.settings.connection_timeout,
+                         json=params)
+        r = r.json()
+        filter_id = r.get("id")
+        return filter_id
 
     def get_filters_list(self) -> dict:
         """
@@ -109,6 +157,33 @@ class Filters(ModuleInterface, LoggingHandler):
                           "distribute": filters.get("distributeBy"),
                           "top": filters.get("top"),
                           "aliases": filters.get("aliases")}
+                }
+
+    def get_filter_info_pdql(self, filter_id: str) -> dict:
+        """
+        Получить информацию по фильтру
+
+        :param filter_id: ID фильтра
+        :return: {"param1": "value", "param2": "value"}
+        """
+        api_filter = self.__api_filter + "/{}"
+        api_url = api_filter.format(filter_id)
+        url = "https://{}{}".format(self.__core_hostname, api_url)
+
+        r = exec_request(self.__core_session,
+                         url,
+                         method='GET',
+                         timeout=self.settings.connection_timeout)
+        filters = r.json()
+
+        self.log.info('status=success, action=get_filter_info_pdql, msg="Got info for filter {}", '
+                      'hostname="{}"'.format(filter_id, self.__core_hostname))
+
+        return {"name": filters.get("name"),
+                "folder_id": filters.get("folderId"),
+                "removed": filters.get("isRemoved"),
+                "source": filters.get("source"),
+                "pdqlQuery": filters.get("pdqlQuery")
                 }
 
     def close(self):
