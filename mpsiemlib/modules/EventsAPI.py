771841a4930b022d42e071df932820c16d87de7f
Gennadiy Mukhamedzyanov <g.mukhamedzyanov@bi.zone>
2025-12-08 18:45:11 +0300

    Переработка модуля EventsAPI

--- mpsiemlib/modules/EventsAPI.py
+++ mpsiemlib/modules/EventsAPI.py
@@ -7,58 +7,56 @@ from mpsiemlib.common import exec_request, get_metrics_start_time, get_metrics_t
 
 class EventsAPI(ModuleInterface, LoggingHandler):
     """Модуль получения информации о событиях через API UI."""
-    __api_events_metadata = '/api/events/v2/events_metadata'
-    __api_event_details = '/api/events/v2/events/{}/normalized?time={}'
-    __api_events_aggregation = '/api/events/v2/events/aggregation?offset=0'
-    __api_events_aggregation_by_asset_group = '/api/events/v2/events/aggregation?offset=0&groupIds={}'
-    __api_events_for_incident = '/api/incidents/events'
-    __api_events = '/api/events/v2/events'
+
+    __api_events_metadata = "/api/events/v2/events_metadata"
+    __api_event_details = "/api/events/v2/events/{}/normalized?time={}"
+    __api_events_aggregation = "/api/events/v2/events/aggregation?offset=0"
+    __api_events_aggregation_by_asset_group = "/api/events/v2/events/aggregation?offset=0&groupIds={}"
+    __api_events_for_incident = "/api/incidents/events"
+    __api_events = "/api/events/v2/events"
     __api_events_v3 = "/api/events/v3/events?limit={}&offset={}"
 
     def __init__(self, auth: MPSIEMAuth, settings: Settings):
         ModuleInterface.__init__(self, auth, settings)
         LoggingHandler.__init__(self)
-        self.__core_session = auth.sessions['core']
+        self.__core_session = auth.sessions["core"]
         self.__core_hostname = auth.creds.core_hostname
         self.log.debug('status=success, action=prepare, msg="EventsAPI Module init"')
 
     def get_events_group_by_api_json(self, params: dict, group_ids=None) -> dict:
         """Получить группировки в формате JSON по фильтру запроса.
 
-        :param: params: Параметры запроса
+        :param: Params: Параметры запроса
         :param: group_ids: Группа
         :return: Словарь с атрибутами активов
         """
-        self.log.debug(
-            'status=prepare, action=get_events_group_by_api_json, msg="Try to aggregate events by query {}", '
-            'hostname="{}"'.format(params['filter']['groupBy'], self.__core_hostname))
+        self.log.debug(f'status=prepare, action=get_events_group_by_api_json, '
+                       f'msg="Try to aggregate events by query {params["filter"]["groupBy"]!r}", '
+                       f'hostname={self.__core_hostname!r}')
 
         if group_ids:
             api_url = self.__api_events_aggregation_by_asset_group.format(group_ids)
-            url = 'https://{}{}'.format(self.__core_hostname, api_url)
+            url = f'https://{self.__core_hostname}{api_url}'
         else:
-            url = 'https://{}{}'.format(self.__core_hostname, self.__api_events_aggregation)
+            url = f'https://{self.__core_hostname}{self.__api_events_aggregation}'
 
         start_time = get_metrics_start_time()
         line_counter = 0
-        rq = exec_request(self.__core_session,
-                          url,
-                          method='POST',
-                          timeout=self.settings.connection_timeout,
-                          json=params)
-        response = rq.json()
+        response = exec_request(
+            self.__core_session,
+            url,
+            method="POST",
+            timeout=self.settings.connection_timeout,
+            json=params).json()
 
         took_time = get_metrics_took_time(start_time)
 
-        self.log.info(
-            'status=success, action=get_events_group_by_api_json, msg="Query executed, response have been read", '
-            'hostname="{}", lines={}'.format(self.__core_hostname, line_counter))
-        self.log.info(
-            'hostname="{}", metric=get_events_group_by_api_json, took={}ms, objects={}'
-            .format(self.__core_hostname,
-                    took_time,
-                    line_counter))
-        return response.get('rows')
+        self.log.info(f'status=success, action=get_events_group_by_api_json, '
+                      f'msg="Query executed, response have been read", hostname={self.__core_hostname!r}, '
+                      f'lines={line_counter!r}')
+        self.log.info(f'hostname={self.__core_hostname!r}, metric=get_events_group_by_api_json, '
+                      f'took={took_time:.4f} ms, objects={line_counter!r}')
+        return response.get("rows")
 
     def get_event_details(self, event_id, event_date) -> dict:
         """Получить событие (все заполненные поля) по его идентификатору и
@@ -72,32 +70,28 @@ class EventsAPI(ModuleInterface, LoggingHandler):
         """
         api_url = self.__api_event_details.format(event_id, event_date)
 
-        url = 'https://{}{}'.format(self.__core_hostname, api_url)
-        rq = exec_request(self.__core_session, url)
-        response = rq.json()
+        url = f'https://{self.__core_hostname}{api_url}'
+        response = exec_request(self.__core_session, url).json()
 
-        if response is None or 'event' not in response:
-            self.log.error('status=failed, action=get_event_details, msg="Core data request return None or '
-                           'has wrong response structure", '
-                           'hostname="{}"'.format(self.__core_hostname))
+        if response is None or "event" not in response:
+            self.log.error(f'status=failed, action=get_event_details, msg="Core data request return None or '
+                           f'has wrong response structure", hostname={self.__core_hostname!r}')
             raise Exception("Core data request return None or has wrong response structure")
-        return response.get('event')
+        return response.get("event")
 
     def get_events_metadata(self):
         """Получить список поддерживаемых полей таксономии событий."""
 
-        url = 'https://{}{}'.format(self.__core_hostname, self.__api_events_metadata)
+        url = f'https://{self.__core_hostname}{self.__api_events_metadata}'
 
-        rq = exec_request(self.__core_session, url)
-        response = rq.json()
+        response = exec_request(self.__core_session, url).json()
 
-        if response is None or 'fields' not in response:
-            self.log.error('status=failed, action=get_events_metadata, msg="Core data request return None or '
-                           'has wrong response structure", '
-                           'hostname="{}"'.format(self.__core_hostname))
+        if response is None or "fields" not in response:
+            self.log.error(f'status=failed, action=get_events_metadata, msg="Core data request return None or '
+                           f'has wrong response structure", hostname={self.__core_hostname!r}')
 
-            raise Exception('Core data request return None or has wrong response structure')
-        return response.get('fields')
+            raise Exception("Core data request return None or has wrong response structure")
+        return response.get("fields")
 
     def get_events_grouped_by_fields(self, query_filter, group_by_fields, time_from, time_to) -> dict[str, int]:
         """Получить события по фильтру, сгруппированные по заданным полям.
@@ -117,52 +111,35 @@ class EventsAPI(ModuleInterface, LoggingHandler):
             "filter": {
                 "select": ["time", "event_src.host", "text"],
                 "where": query_filter,
-                "orderBy": [{
-                    "field": "time",
-                    "sortOrder": "descending"
-                }
-                ],
+                "orderBy": [{"field": "time", "sortOrder": "descending"}],
                 "groupBy": group_by_fields,
-                "aggregateBy": [{
-                    "function": "COUNT",
-                    "field": "*",
-                    "unique": false
-                }
-                ],
+                "aggregateBy": [{"function": "COUNT", "field": "*", "unique": false}],
                 "distributeBy": [],
                 "top": 10000,
-                "aliases": {
-                    "groupBy": {},
-                    "aggregateBy": {
-                        "COUNT": "Cnt"
-                    }
-                },
+                "aliases": {"groupBy": {}, "aggregateBy": {"COUNT": "Cnt"}},
                 "searchType": null,
                 "searchSources": null,
                 "localSources": null,
-                "groupByOrder": [{
-                    "field": "count",
-                    "sortOrder": "Descending"
-                }
-                ],
-                "showNullGroups": true
+                "groupByOrder": [{"field": "count", "sortOrder": "Descending"}],
+                "showNullGroups": true,
             },
             "timeFrom": time_from,
-            "timeTo": time_to
+            "timeTo": time_to,
         }
         api_url = self.__api_events_aggregation
         url = f'https://{self.__core_hostname}{api_url}'
 
-        rq = exec_request(self.__core_session, url, method='POST', json=params)
-        response = rq.json()
+        response = exec_request(self.__core_session, url, method="POST", json=params).json()
 
-        if response is None or 'rows' not in response:
-            self.log.error('status=failed, action=get_events_grouped_by_fields, msg="Core data request return None or '
-                           'has wrong response structure", '
-                           'hostname="{}"'.format(self.__core_hostname))
-            raise Exception('Core data request return None or has wrong response structure')
+        if response is None or "rows" not in response:
+            self.log.error(f'status=failed, action=get_events_grouped_by_fields, msg="Core data request return None or '
+                           f'has wrong response structure", hostname={self.__core_hostname}')
+            raise Exception("Core data request return None or has wrong response structure")
 
-        return {' | '.join(str(s) for s in e['groups']): int(e['values'][0]) for e in response['rows']}
+        return {
+            " | ".join(str(s) for s in e["groups"]): int(e["values"][0])
+            for e in response["rows"]
+        }
 
     def get_events_by_filter(self, pdql_filter, fields, time_from, time_to, limit, offset) -> dict:
         """Получить события по фильтру.
@@ -179,39 +156,30 @@ class EventsAPI(ModuleInterface, LoggingHandler):
         """
         null = None
         params = {
-            'filter': {
-                'select': fields,
-                'where': f'{pdql_filter}',
-                'orderBy': [
-                    {
-                        'field': 'time',
-                        'sortOrder': 'ascending'
-                    }
-                ],
-                'groupBy': [],
-                'aggregateBy': [],
-                'distributeBy': [],
-                'top': null,
-                'aliases': {
-                    'groupBy': {}
-                }
+            "filter": {
+                "select": fields,
+                "where": f"{pdql_filter}",
+                "orderBy": [{"field": "time", "sortOrder": "ascending"}],
+                "groupBy": [],
+                "aggregateBy": [],
+                "distributeBy": [],
+                "top": null,
+                "aliases": {"groupBy": {}},
             },
-            'groupValues': [],
-            'timeFrom': time_from,
-            'timeTo': time_to
+            "groupValues": [],
+            "timeFrom": time_from,
+            "timeTo": time_to,
         }
         api_url = self.__api_events.format(limit, offset)
         url = f'https://{self.__core_hostname}{api_url}'
 
-        rq = exec_request(self.__core_session, url, method='POST', json=params)
-        response = rq.json()
+        response = exec_request(self.__core_session, url, method="POST", json=params).json()
 
-        if response is None or 'events' not in response:
-            self.log.error('status=failed, action=get_events_by_filter, msg="Core data request return None or '
-                           'has wrong response structure", '
-                           'hostname="{}"'.format(self.__core_hostname))
-            raise Exception('Core data request return None or has wrong response structure')
-        return response.get('events')
+        if response is None or "events" not in response:
+            self.log.error(f'status=failed, action=get_events_by_filter, msg="Core data request return None or '
+                           f'has wrong response structure", hostname={self.__core_hostname!r}')
+            raise Exception("Core data request return None or has wrong response structure")
+        return response.get("events")
 
     def get_events_for_incident(self, fields, incident_id, time_from, time_to, limit, offset):
         """Получить события, связанные с инцидентом.
@@ -228,25 +196,20 @@ class EventsAPI(ModuleInterface, LoggingHandler):
         """
         null = None
         params = {
-            'filter': {
-                'select': fields,
-                'where': '',
-                'orderBy': [
-                    {
-                        'field': 'time',
-                        'sortOrder': 'descending'
-                    }
-                ],
-                'groupBy': [],
-                'aggregateBy': [],
-                'distributeBy': [],
-                'top': null,
-                'aliases': {},
-                'searchType': null,
-                'searchSources': null
+            "filter": {
+                "select": fields,
+                "where": "",
+                "orderBy": [{"field": "time", "sortOrder": "descending"}],
+                "groupBy": [],
+                "aggregateBy": [],
+                "distributeBy": [],
+                "top": null,
+                "aliases": {},
+                "searchType": null,
+                "searchSources": null,
             },
-            'timeFrom': time_from,
-            'timeTo': time_to
+            "timeFrom": time_from,
+            "timeTo": time_to,
         }
 
         api_url = self.__api_events_for_incident.format(incident_id, limit, offset)
@@ -254,15 +217,21 @@ class EventsAPI(ModuleInterface, LoggingHandler):
 
         rq = exec_request(self.__core_session, url, method="POST", json=params)
         response = rq.json()
-        if response is None or 'events' not in response:
-            self.log.error('status=failed, action=get_events_for_incident, msg="Core data request return None or '
-                           'has wrong response structure", '
-                           'hostname="{}"'.format(self.__core_hostname))
-            raise Exception('Core data request return None or has wrong response structure')
-        return response.get('events')
+        if response is None or "events" not in response:
+            self.log.error(f'status=failed, action=get_events_for_incident, msg="Core data request return None or '
+                           f'has wrong response structure", '
+                           f'hostname={self.__core_hostname!r}')
+            raise Exception("Core data request return None or has wrong response structure")
+        return response.get("events")
 
-    def __get_events_by_filter_v3(self, query_filter: str, time_from: int, time_to: int, offset: int, limit: int,
-                                  token: str = None) -> dict:
+    def __get_events_by_filter_v3(
+            self,
+            query_filter: str,
+            time_from: int,
+            time_to: int,
+            offset: int,
+            limit: int,
+            token: str = None) -> dict:
         """
         Получить события по фильтру
         Args:
@@ -275,25 +244,23 @@ class EventsAPI(ModuleInterface, LoggingHandler):
         Returns:
             [dict]: массив событий
         """
-        null = None
         params = {
-            'filter': query_filter,
-            'groupValues': ['1'],
-            'timeFrom': time_from,
-            'timeTo': time_to
+            "filter": query_filter,
+            "groupValues": ["1"],
+            "timeFrom": time_from,
+            "timeTo": time_to,
         }
         api_url = self.__api_events_v3.format(limit, offset)
-        url = 'https://{}{}'.format(self.__core_hostname, api_url)
+        url = f'https://{self.__core_hostname}{api_url}'
         if token is not None:
-            url += '&token={}'.format(token)
+            url += f'&token={token}'
 
-        rq = exec_request(self.__core_session, url, method='POST', json=params)
-        response = rq.json()
+        response = exec_request(self.__core_session, url, method="POST", json=params).json()
 
-        if response is None or 'events' not in response:
-            self.log.error('status=failed, action=get_events_by_filter, msg="Core data request return None or '
-                           'has wrong response structure", '
-                           'hostname="{}"'.format(self.__core_hostname))
+        if response is None or "events" not in response:
+            self.log.error(f'status=failed, action=get_events_by_filter, msg="Core data request return None or '
+                           f'has wrong response structure", '
+                           f'hostname={self.__core_hostname!r}')
             raise Exception("Core data request return None or has wrong response structure")
         return response  # .get("events")
 
@@ -306,11 +273,9 @@ class EventsAPI(ModuleInterface, LoggingHandler):
         Yields:
             Iterator[dict]: Итератор
         """
-        self.log.debug('status=prepare, action=get_events_by_filter_v3, msg="Try to get events", '
-                       'hostname="{}", filter="{}", begin={}, end={}'.format(self.__core_hostname,
-                                                                             query_filter,
-                                                                             time_from,
-                                                                             time_to))
+        self.log.debug(f'status=prepare, action=get_events_by_filter_v3, msg="Try to get events", '
+                       f'hostname={self.__core_hostname!r}, filter={query_filter!r}, '
+                       f'begin={time_from!r}, end={time_to!r}')
         # Пачками выгружаем содержимое
         is_end = False
         offset = 0
@@ -319,9 +284,13 @@ class EventsAPI(ModuleInterface, LoggingHandler):
         line_counter = 0
         start_time = get_metrics_start_time()
         while not is_end:
-            ret = self.__get_events_by_filter_v3(query_filter, time_from, time_to, offset, limit, token)
-            events = ret.get('events')
-            token = ret.get('token')  # последующие запросы с токеном должны быстрее, чем без него
+            ret = self.__get_events_by_filter_v3(
+                query_filter, time_from, time_to, offset, limit, token
+            )
+            events = ret.get("events")
+            token = ret.get(
+                "token"
+            )  # последующие запросы с токеном должны быстрее, чем без него
             if len(events) < limit:
                 is_end = True
             offset += limit
@@ -329,7 +298,5 @@ class EventsAPI(ModuleInterface, LoggingHandler):
                 line_counter += 1
                 yield i
         took_time = get_metrics_took_time(start_time)
-        self.log.info(
-            'hostname="{}", metric=get_events_by_filter_v3, took={}ms, objects={}'.format(self.__core_hostname,
-                                                                                          took_time,
-                                                                                          line_counter))
+        self.log.info(f'hostname={self.__core_hostname!r}, metric=get_events_by_filter_v3, '
+                      f'took={took_time:.4f} ms, objects={line_counter!r}')
--- tests/unit_events_api.py
+++ tests/unit_events_api.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 from datetime import datetime
 
@@ -5,13 +6,13 @@ import pytz
 
 from mpsiemlib.common import *
 from mpsiemlib.modules import MPSIEMWorker
-from tests.settings import creds_local, settings
+from settings import settings, creds_pat
 
 
 class TestEventsAPITestCase(unittest.TestCase):
     __mpsiemworker = None
     __module = None
-    __creds = creds_local
+    __creds = creds_pat
     __settings = settings
     __begin = 0
     __end = 0
@@ -19,22 +20,36 @@ class TestEventsAPITestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.__mpsiemworker = MPSIEMWorker(cls.__creds, cls.__settings)
-        cls.__module = cls.__mpsiemworker.get_module(ModuleNames.EVENTSAPI)
+        cls.__module = cls.__mpsiemworker.get_module(ModuleNames.EVENTSAPI)  # noqa
         cls.__end = round(datetime.now(tz=pytz.timezone(settings.local_timezone)).timestamp())
-        cls.__begin = cls.__end - 86400
+        cls.__begin = cls.__end - 60
 
     @classmethod
     def tearDownClass(cls) -> None:
         cls.__module.close()
-        
+
     def test_get_events_grouped_by_fields(self):
         grouping_fields = ['id']
         result = self.__module.get_events_grouped_by_fields(
             query_filter='normalized = true',
             group_by_fields=grouping_fields,
-            time_from=self.__begin, 
-            time_to=self.__end
-        )
+            time_from=self.__begin,
+            time_to=self.__end)
+        print(json.dumps(result, indent=4, ensure_ascii=False))
+        self.assertGreater(len(result), 0)
+
+    def test_get_events_metadata(self):
+        result = self.__module.get_events_metadata()
+        print(json.dumps(result, indent=4, ensure_ascii=False))
+        self.assertGreater(len(result), 0)
+
+    def test_get_events_by_filter_v3(self):
+        query_filter = 'select(time, event_src.host, text) | sort(time desc)'
+        time_from = self.__begin
+        time_to = self.__end
+        result = list(self.__module.get_events_by_filter_v3(query_filter=query_filter,
+                                                            time_from=time_from, time_to=time_to))
+        print(json.dumps(result, indent=4, ensure_ascii=False))
         self.assertGreater(len(result), 0)
 
 
