f780d4340e0fa5b62b5e0631342323fb00108f1f
Gennadiy Mukhamedzyanov <g.mukhamedzyanov@bi.zone>
2025-12-10 15:33:35 +0300

    Переработка модуля HealthMonitor

--- mpsiemlib/modules/HealthMonitor.py
+++ mpsiemlib/modules/HealthMonitor.py
@@ -1,43 +1,51 @@
 from typing import List
 
-from mpsiemlib.common import ModuleInterface, MPSIEMAuth, LoggingHandler, MPComponents, Settings
+from mpsiemlib.common import (
+    ModuleInterface,
+    MPSIEMAuth,
+    LoggingHandler,
+    MPComponents,
+    Settings,
+)
 from mpsiemlib.common import exec_request
 
 
 class HealthMonitor(ModuleInterface, LoggingHandler):
     """Health monitor module."""
 
-    __api_global_status = '/api/health_monitoring/v2/total_status'
-    __api_checks = '/api/health_monitoring/v2/checks?limit={}&offset={}'
-    __api_license_status = '/api/licensing/v2/license_validity'
-    __api_agents_status = '/api/components/agent'
-    __api_agents_status_new = '/api/v1/scanner_agents'
-    __api_kb_status = '/api/v1/knowledgeBase'
+    __api_global_status = "/api/health_monitoring/v2/total_status"
+    __api_checks = "/api/health_monitoring/v2/checks?limit={}&offset={}"
+    __api_license_status = "/api/licensing/v2/license_validity"
+    __api_licence_status_r27 = "/api/licensing/v4/licenses"
+    __api_agents_status = "/api/components/agent"
+    __api_agents_status_new = "/api/v1/scanner_agents"
+    __api_kb_status = "/api/v1/knowledgeBase"
     __kb_port = 8091
+    __mc_port = 3334
 
     def __init__(self, auth: MPSIEMAuth, settings: Settings):
         ModuleInterface.__init__(self, auth, settings)
         LoggingHandler.__init__(self)
-        self.__core_session = auth.sessions['core']
+        self.__core_session = auth.sessions["core"]
         self.__core_hostname = auth.creds.core_hostname
         self.__core_version = auth.get_core_version()
-        self.__kb_session = auth.connect(MPComponents.KB)
+        # self.__kb_session = auth.connect(MPComponents.KB)  # noqa
 
     def get_health_status(self) -> str:
         """Получить общее состояние системы.
 
         :return: "ok" - если нет ошибок
         """
-        url = f'https://{self.__core_hostname}{self.__api_global_status}'
-        r = exec_request(self.__core_session,
-                         url,
-                         method='GET',
-                         timeout=self.settings.connection_timeout)
-        response = r.json()
-        status = response.get('status')
+        url = f"https://{self.__core_hostname}{self.__api_global_status}"
+        response = exec_request(
+            self.__core_session,
+            url,
+            method="GET",
+            timeout=self.settings.connection_timeout).json()
+        status = response.get("status")
 
-        self.log.info('status=success, action=get_health_status, msg="Got global status", '
-                      'hostname="{}" status="{}"'.format(self.__core_hostname, status))
+        self.log.info(f'status=success, action=get_health_status, msg="Got global status", '
+                      f'hostname={self.__core_hostname!r} status={status!r}')
 
         return status
 
@@ -49,32 +57,35 @@ class HealthMonitor(ModuleInterface, LoggingHandler):
         limit = 1000
         offset = 0
         api_url = self.__api_checks.format(limit, offset)
-        url = f'https://{self.__core_hostname}{api_url}'
-        r = exec_request(self.__core_session,
-                         url,
-                         method='GET',
-                         timeout=self.settings.connection_timeout)
-        response = r.json()
-        errors = response.get('items')
+        url = f"https://{self.__core_hostname}{api_url}"
+        response = exec_request(
+            self.__core_session,
+            url,
+            method="GET",
+            timeout=self.settings.connection_timeout).json()
+        errors = response.get("items")
 
         ret = []
         for i in errors:
-            source = i.get('source') if i.get('source') is not None else {}
-            params = i.get('parameters') if i.get('parameters') is not None else {}
-            ret.append({'id': i.get('id'),
-                        'timestamp': i.get('timestamp'),
-                        'status': i.get('status', '').lower(),
-                        'type': i.get('type', '').lower(),
-                        'name': source.get('displayName').lower(),
-                        'hostname': source.get('hostName'),
-                        'ip': source.get('ipAddresses'),
-                        'component_name': params.get('componentName'),
-                        'component_hostname': params.get('hostName'),
-                        'component_ip': params.get('ipAddresses')
-                        })
+            source = i.get("source") if i.get("source") is not None else {}
+            params = i.get("parameters") if i.get("parameters") is not None else {}
+            ret.append(
+                {
+                    "id": i.get("id"),
+                    "timestamp": i.get("timestamp"),
+                    "status": i.get("status", "").lower(),
+                    "type": i.get("type", "").lower(),
+                    "name": source.get("displayName").lower(),
+                    "hostname": source.get("hostName"),
+                    "ip": source.get("ipAddresses"),
+                    "component_name": params.get("componentName"),
+                    "component_hostname": params.get("hostName"),
+                    "component_ip": params.get("ipAddresses"),
+                }
+            )
 
-        self.log.info('status=success, action=get_health_errors, msg="Got errors", '
-                      'hostname="{}" count="{}"'.format(self.__core_hostname, len(errors)))
+        self.log.info(f'status=success, action=get_health_errors, msg="Got errors", '
+                      f'hostname={self.__core_hostname!r} count={len(errors)!r}')
 
         return ret
 
@@ -83,22 +94,42 @@ class HealthMonitor(ModuleInterface, LoggingHandler):
 
         :return: Dict
         """
-        url = f'https://{self.__core_hostname}{self.__api_license_status}'
-        r = exec_request(self.__core_session,
-                         url,
-                         method='GET',
-                         timeout=self.settings.connection_timeout)
-        response = r.json()
-        lic = response.get('license')
-        status = {'valid': response.get('validity') == 'valid',
-                  'key': lic.get('keyNumber'),
-                  'type': lic.get('licenseType'),
-                  'granted': lic.get('keyDate'),
-                  'expiration': lic.get('expirationDate'),
-                  'assets': lic.get('assetsCount')}
+        if int(self.__core_version.split(".")[0]) < 27:
+            url = f"https://{self.__core_hostname}{self.__api_license_status}"
+        else:
+            url = f"https://{self.__core_hostname}:{self.__mc_port}{self.__api_licence_status_r27}"
 
-        self.log.info('status=success, action=get_health_license_status, msg="Got license status", '
-                      'hostname="{}"'.format(self.__core_hostname))
+        response = exec_request(
+            self.__core_session,
+            url,
+            method="GET",
+            timeout=self.settings.connection_timeout).json()
+
+        if int(self.__core_version.split(".")[0]) < 27:
+            lic = response.get("license")
+
+            status = {
+                'valid': response.get('validity') == 'valid',
+                'key': lic.get('keyNumber'),
+                'type': lic.get('licenseType'),
+                'granted': lic.get('keyDate'),
+                'expiration': lic.get('expirationDate'),
+                'assets': lic.get('assetsCount'),
+            }
+        else:
+            lic = response[0].get('license')
+            status = {
+                'id': lic.get('id'),
+                'key': lic.get('licenseFile').get('licenseNumber'),
+                'type': lic.get('licenseFile').get('general').get('licenseType'),
+                'mode': lic.get("licenseFile").get("general").get('licenseMode'),
+                'current_eps': lic.get('licenseFile').get('workloads')[0].get('current'),
+                'eps': lic.get('licenseFile').get('workloads')[0].get('quantity'),
+                'expiration': lic.get('gracePeriodExpiredTime')
+            }
+
+        self.log.info(f'status=success, action=get_health_license_status, msg="Got license status", '
+                      f'hostname={self.__core_hostname!r}')
 
         return status
 
@@ -107,34 +138,36 @@ class HealthMonitor(ModuleInterface, LoggingHandler):
 
         :return: Список агентов и их параметры.
         """
-        if int(self.__core_version.split('.')[0]) < 25:
-            url = "https://{}{}".format(self.__core_hostname, self.__api_agents_status)
+        if int(self.__core_version.split(".")[0]) < 25:
+            url = f"https://{self.__core_hostname}{self.__api_agents_status}"
         else:
-            url = "https://{}{}".format(self.__core_hostname, self.__api_agents_status_new)
+            url = f"https://{self.__core_hostname}{self.__api_agents_status_new}"
 
-        r = exec_request(self.__core_session,
-                         url,
-                         method='GET',
-                         timeout=self.settings.connection_timeout)
-        response = r.json()
+        response = exec_request(
+            self.__core_session,
+            url,
+            method="GET",
+            timeout=self.settings.connection_timeout).json()
 
         agents = []
         for i in response:
-            agents.append({
-                'id': i.get('id'),
-                'name': i.get('name'),
-                'hostname': i.get('address'),
-                'version': i.get('version'),
-                'updates': i.get('availableUpdates'),
-                'status': i.get('status'),
-                'roles': i.get('roleNames'),
-                'ip': i.get('ipAddresses'),
-                'platform': i.get('platform'),
-                'modules': i.get('modules')
-            })
+            agents.append(
+                {
+                    "id": i.get("id"),
+                    "name": i.get("name"),
+                    "hostname": i.get("address"),
+                    "version": i.get("version"),
+                    "updates": i.get("availableUpdates"),
+                    "status": i.get("status"),
+                    "roles": i.get("roleNames"),
+                    "ip": i.get("ipAddresses"),
+                    "platform": i.get("platform"),
+                    "modules": i.get("modules"),
+                }
+            )
 
-        self.log.info('status=success, action=get_health_agents_status, msg="Got agents status", '
-                      'hostname="{}" count="{}"'.format(self.__core_hostname, len(agents)))
+        self.log.info(f'status=success, action=get_health_agents_status, msg="Got agents status", '
+                      f'hostname={self.__core_hostname!r} count={len(agents)!r}')
 
         return agents
 
@@ -143,22 +176,25 @@ class HealthMonitor(ModuleInterface, LoggingHandler):
 
         :return: dict.
         """
-        url = f'https://{self.__core_hostname}:{self.__kb_port}{self.__api_kb_status}'
-        r = exec_request(self.__kb_session,
-                         url,
-                         method='GET',
-                         timeout=self.settings.connection_timeout)
-        response = r.json()
-        local = response.get('localKnowledgeBase')
-        remote = response.get('remoteKnowledgeBase')
-        status = {'status': response.get('status'),
-                  'local_updated': local.get('lastUpdate'),
-                  'local_current_revision': local.get('localRevision'),
-                  'local_global_revision': local.get('globalRevision'),
-                  'kb_db_name': remote.get('name')}
+        url = f"https://{self.__core_hostname}:{self.__kb_port}{self.__api_kb_status}"
+        response = exec_request(
+            self.__core_session,
+            url,
+            method="GET",
+            timeout=self.settings.connection_timeout).json()
 
-        self.log.info('status=success, action=get_health_kb_status, msg="Got KB status", '
-                      'hostname="{}"'.format(self.__core_hostname))
+        local = response.get("localKnowledgeBase")
+        remote = response.get("remoteKnowledgeBase")
+        status = {
+            "status": response.get("status"),
+            "local_updated": local.get("lastUpdate"),
+            "local_current_revision": local.get("localRevision"),
+            "local_global_revision": local.get("globalRevision"),
+            "kb_db_name": remote.get("name"),
+        }
+
+        self.log.info(f'status=success, action=get_health_kb_status, msg="Got KB status", '
+                      f'hostname={self.__core_hostname!r}')
 
         return status
 
--- tests/unit_healthmonitor.py
+++ tests/unit_healthmonitor.py
@@ -1,20 +1,21 @@
+import json
 import unittest
 
 from mpsiemlib.common import *
 from mpsiemlib.modules import MPSIEMWorker
-from tests.settings import creds, settings
+from settings import settings, creds_pat
 
 
 class KBTestCase(unittest.TestCase):
     __mpsiemworker = None
     __module = None
-    __creds_ldap = creds
+    __creds = creds_pat
     __settings = settings
 
     @classmethod
     def setUpClass(cls) -> None:
-        cls.__mpsiemworker = MPSIEMWorker(cls.__creds_ldap, cls.__settings)
-        cls.__module = cls.__mpsiemworker.get_module(ModuleNames.HEALTH)
+        cls.__mpsiemworker = MPSIEMWorker(cls.__creds, cls.__settings)
+        cls.__module = cls.__mpsiemworker.get_module(ModuleNames.HEALTH)    # noqa
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -22,28 +23,38 @@ class KBTestCase(unittest.TestCase):
 
     def test_get_global_status(self):
         ret = self.__module.get_health_status()
-        # self.assertTrue(type(ret) == str)
+
+        print(json.dumps(ret, indent=4, ensure_ascii=False))
+
         self.assertIsInstance(ret, str)
 
     def test_get_errors(self):
         ret = self.__module.get_health_errors()
-        # self.assertTrue(type(ret) == list)
+
+        print(json.dumps(ret, indent=4, ensure_ascii=False))
+
         self.assertIsInstance(ret, list)
 
     def test_get_license_status(self):
         ret = self.__module.get_health_license_status()
-        # self.assertTrue(len(ret) != 0)
+
+        print(json.dumps(ret, indent=4, ensure_ascii=False))
+
         self.assertGreater(len(ret), 0)
 
     def test_get_agents_status(self):
         ret = self.__module.get_health_agents_status()
-        # self.assertTrue(len(ret) != 0)
+
+        print(json.dumps(ret, indent=4, ensure_ascii=False))
+
         self.assertGreater(len(ret), 0)
 
     @unittest.skip('Not implemented')
     def test_get_kb_status(self):
         ret = self.__module.get_health_kb_status()
-        # self.assertTrue(len(ret) != 0)
+
+        print(json.dumps(ret, indent=4, ensure_ascii=False))
+
         self.assertGreater(len(ret), 0)
 
 
