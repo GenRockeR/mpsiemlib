157f890734b8f4f8231978410a9356dba524b76a
Gennadiy Mukhamedzyanov <g.mukhamedzyanov@bi.zone>
2025-12-10 14:25:38 +0300

    Переработка модуля Filters

--- mpsiemlib/modules/Filters.py
+++ mpsiemlib/modules/Filters.py
@@ -1,4 +1,4 @@
-from mpsiemlib.common import ModuleInterface, MPSIEMAuth, LoggingHandler, MPComponents, Settings
+from mpsiemlib.common import ModuleInterface, MPSIEMAuth, LoggingHandler, Settings
 from mpsiemlib.common import exec_request
 
 
@@ -14,7 +14,7 @@ class Filters(ModuleInterface, LoggingHandler):
     def __init__(self, auth: MPSIEMAuth, settings: Settings):
         ModuleInterface.__init__(self, auth, settings)
         LoggingHandler.__init__(self)
-        self.__core_session = auth.sessions['core']
+        self.__core_session = auth.sessions["core"]
         self.__core_hostname = auth.creds.core_hostname
         self.__core_version = auth.get_core_version()
         self.__folders = {}
@@ -30,18 +30,18 @@ class Filters(ModuleInterface, LoggingHandler):
         if len(self.__folders) != 0:
             return self.__folders
 
-        url = f'https://{self.__core_hostname}{self.__api_filters_list}'
+        url = f"https://{self.__core_hostname}{self.__api_filters_list}"
 
-        r = exec_request(self.__core_session,
-                         url,
-                         method='GET',
-                         timeout=self.settings.connection_timeout)
-        filters = r.json()
+        filters = exec_request(
+            self.__core_session,
+            url,
+            method="GET",
+            timeout=self.settings.connection_timeout).json()
 
         self.__iterate_folders_tree(filters.get("roots"))
 
-        self.log.info('status=success, action=get_folders_list, msg="Got {} folders", '
-                      'hostname="{}"'.format(len(self.__folders), self.__core_hostname))
+        self.log.info(f'status=success, action=get_folders_list, msg="Got {len(self.__folders)!r} folders", '
+                      f'hostname={self.__core_hostname!r}')
 
         return self.__folders
 
@@ -53,68 +53,77 @@ class Filters(ModuleInterface, LoggingHandler):
         """
 
         api_url = self.__api_folder
-        url = "https://{}{}".format(self.__core_hostname, api_url)
-        params = {'name': folder_name, 'parentId': parent_id}
-        r = exec_request(self.__core_session,
-                         url,
-                         method='POST',
-                         timeout=self.settings.connection_timeout,
-                         json=params)
-        r = r.json()
-        folder_id = r.get("folderId")
+        url = f"https://{self.__core_hostname}{api_url}"
+        params = {"name": folder_name, "parentId": parent_id}
+        response = exec_request(
+            self.__core_session,
+            url,
+            method="POST",
+            timeout=self.settings.connection_timeout,
+            json=params).json()
+
+        folder_id = response.get("folderId")
         return folder_id
 
     def create_event_filter_v2(self, filter_name: str, folder_id: str, params: dict) -> str:
-        """Создать директорию для фильтров :param filter_name: имя создаваемого
-        фильтра :param folder_id: ID директории, в которой создается фильтр
-        :param params: поисковой запрос.
+        """Создать директорию для фильтров
+        :param filter_name: имя создаваемого фильтра
+        :param folder_id: ID директории, в которой создается фильтр
+        :param params: поисковый запрос.
 
         :return: filter_id: ID созданного фильтра
         """
 
         api_url = self.__api_filter_v2
-        url = "https://{}{}".format(self.__core_hostname, api_url)
-        params['folderId'] = folder_id
-        params['name'] = filter_name
-        r = exec_request(self.__core_session,
-                         url,
-                         method='POST',
-                         timeout=self.settings.connection_timeout,
-                         json=params)
-        r = r.json()
-        filter_id = r.get("id")
-        self.log.info('status=success, action=create_event_filter_v3, msg="Created filter {}", '
-                      'hostname="{}"'.format(filter_id, self.__core_hostname))
+        url = f"https://{self.__core_hostname}{api_url}"
+        params["folderId"] = folder_id
+        params["name"] = filter_name
+        response = exec_request(
+            self.__core_session,
+            url,
+            method="POST",
+            timeout=self.settings.connection_timeout,
+            json=params).json()
+
+        filter_id = response.get("id")
+        self.log.info(f'status=success, action=create_event_filter_v3, msg="Created filter {filter_id!r}", '
+                      f'hostname={self.__core_hostname!r}')
         return filter_id
 
     def create_event_filter_v3(self, filter_name: str, folder_id: str, pdql_query: str) -> str:
-        """Создать директорию для фильтров :param filter_name: имя создаваемого
-        фильтра :param folder_id: ID директории, в которой создается фильтр
-        :param pdql_query: поисковой запрос.
+        """Создать директорию для фильтров
+        :param filter_name: имя создаваемого фильтра
+        :param folder_id: ID директории, в которой создается фильтр
+        :param pdql_query: поисковый запрос.
 
         :return: filter_id: ID созданного фильтра
         """
 
         api_url = self.__api_filter_v3
-        url = "https://{}{}".format(self.__core_hostname, api_url)
-        params = {'folderId': folder_id, 'name': filter_name, 'pdqlQuery': pdql_query}
-        r = exec_request(self.__core_session,
-                         url,
-                         method='POST',
-                         timeout=self.settings.connection_timeout,
-                         json=params)
-        r = r.json()
-        filter_id = r.get("id")
-        self.log.info('status=success, action=create_event_filter_v3, msg="Created filter {}", '
-                      'hostname="{}"'.format(filter_id, self.__core_hostname))
+        url = f"https://{self.__core_hostname}{api_url}"
+        params = {"folderId": folder_id, "name": filter_name, "pdqlQuery": pdql_query}
+        response = exec_request(
+            self.__core_session,
+            url,
+            method="POST",
+            timeout=self.settings.connection_timeout,
+            json=params).json()
+        filter_id = response.get("id")
+        self.log.info(f'status=success, action=create_event_filter_v3, msg="Created filter {filter_id!r}", '
+                      f'hostname={self.__core_hostname!r}')
         return filter_id
 
     def create_event_filter(self, filter_name: str, folder_id: str, pdql_query: str) -> str:
-        if float(f"{self.__core_version.split('.')[0]}.{self.__core_version.split('.')[1]}") < 26.1:
-            self.log.debug('Using create_event_filter_v2')
+        if (
+                float(
+                    f"{self.__core_version.split('.')[0]}.{self.__core_version.split('.')[1]}"
+                )
+                < 26.1
+        ):
+            self.log.debug("Using create_event_filter_v2")
             return self.create_event_filter_v2(filter_name, folder_id, pdql_query)
         else:
-            self.log.debug('Using create_event_filter_v3')
+            self.log.debug("Using create_event_filter_v3")
             return self.create_event_filter_v3(filter_name, folder_id, pdql_query)
 
     def get_filters_list(self) -> dict:
@@ -129,36 +138,45 @@ class Filters(ModuleInterface, LoggingHandler):
         # папки и фильтры лежат в одной структуре и парсятся совместно
         self.get_folders_list()
 
-        self.log.info('status=success, action=get_filters_list, msg="Got {} filters", '
-                      'hostname="{}"'.format(len(self.__filters), self.__core_hostname))
+        self.log.info(f'status=success, action=get_filters_list, msg="Got {len(self.__filters)!r} filters", '
+                      f'hostname={self.__core_hostname!r}')
 
         return self.__filters
 
     def __iterate_folders_tree(self, root_node, parent_id=None):
         for i in root_node:
-            node_id = i.get('id')
-            node_name = i.get('name')
-            node_source = i.get('meta', {}).get('source')
-            if i.get('type') == 'filter_node':
-                self.__filters[node_id] = {'folder_id': parent_id,
-                                           'name': node_name,
-                                           'source': node_source}
+            node_id = i.get("id")
+            node_name = i.get("name")
+            node_source = i.get("meta", {}).get("source")
+            if i.get("type") == "filter_node":
+                self.__filters[node_id] = {
+                    "folder_id": parent_id,
+                    "name": node_name,
+                    "source": node_source,
+                }
                 continue
-            if i.get('type') == 'folder_node':
-                self.__folders[node_id] = {'parent_id': parent_id,
-                                           'name': node_name,
-                                           'source': node_source}
-                node_children = i.get('children')
+            if i.get("type") == "folder_node":
+                self.__folders[node_id] = {
+                    "parent_id": parent_id,
+                    "name": node_name,
+                    "source": node_source,
+                }
+                node_children = i.get("children")
                 if node_children is not None and len(node_children) != 0:
                     self.__iterate_folders_tree(node_children, node_id)
 
     def get_filter_info(self, filter_id: str) -> dict:
-        self.log.debug(f'Current core version: {self.__core_version}')
-        if float(f"{self.__core_version.split('.')[0]}.{self.__core_version.split('.')[1]}") < 26.1:
-            self.log.debug('Using get_filter_info_v2')
+        self.log.debug(f"Current core version: {self.__core_version!r}")
+        if (
+                float(
+                    f"{self.__core_version.split('.')[0]}.{self.__core_version.split('.')[1]}"
+                )
+                < 26.1
+        ):
+            self.log.debug("Using get_filter_info_v2")
             return self.get_filter_info_v2(filter_id)
         else:
-            self.log.debug('Using get_filter_info_v3')
+            self.log.debug("Using get_filter_info_v3")
             return self.get_filter_info_v3(filter_id)
 
     def get_filter_info_v2(self, filter_id: str) -> dict:
@@ -168,29 +186,33 @@ class Filters(ModuleInterface, LoggingHandler):
         :return: {"param1": "value", "param2": "value"}
         """
         api_url = self.__api_filter_info.format(filter_id)
-        url = f'https://{self.__core_hostname}{api_url}'
+        url = f"https://{self.__core_hostname}{api_url}"
 
-        r = exec_request(self.__core_session,
-                         url,
-                         method='GET',
-                         timeout=self.settings.connection_timeout)
-        filters = r.json()
+        filters = exec_request(
+            self.__core_session,
+            url,
+            method="GET",
+            timeout=self.settings.connection_timeout).json()
 
-        self.log.info('status=success, action=get_filter_info_v2, msg="Got info for filter {}", '
-                      'hostname="{}"'.format(filter_id, self.__core_hostname))
+        self.log.info(f'status=success, action=get_filter_info_v2, msg="Got info for filter {filter_id!r}", '
+                      f'hostname={self.__core_hostname!r}')
 
-        return {"name": filters.get("name"),
-                "folder_id": filters.get("folderId"),
-                "removed": filters.get("isRemoved"),
-                "source": filters.get("source"),
-                "query": {"select": filters.get("select"),
-                          "where": filters.get("where"),
-                          "group": filters.get("groupBy"),
-                          "order": filters.get("orderBy"),
-                          "aggregate": filters.get("aggregateBy"),
-                          "distribute": filters.get("distributeBy"),
-                          "top": filters.get("top"),
-                          "aliases": filters.get("aliases")}}
+        return {
+            "name": filters.get("name"),
+            "folder_id": filters.get("folderId"),
+            "removed": filters.get("isRemoved"),
+            "source": filters.get("source"),
+            "query": {
+                "select": filters.get("select"),
+                "where": filters.get("where"),
+                "group": filters.get("groupBy"),
+                "order": filters.get("orderBy"),
+                "aggregate": filters.get("aggregateBy"),
+                "distribute": filters.get("distributeBy"),
+                "top": filters.get("top"),
+                "aliases": filters.get("aliases"),
+            },
+        }
 
     def get_filter_info_v3(self, filter_id: str) -> dict:
         """Получить информацию по фильтру.
@@ -200,23 +222,24 @@ class Filters(ModuleInterface, LoggingHandler):
         """
         api_filter = self.__api_filter_v3 + "/{}"
         api_url = api_filter.format(filter_id)
-        url = "https://{}{}".format(self.__core_hostname, api_url)
+        url = f"https://{self.__core_hostname}{api_url}"
 
-        r = exec_request(self.__core_session,
-                         url,
-                         method='GET',
-                         timeout=self.settings.connection_timeout)
-        filters = r.json()
+        filters = exec_request(
+            self.__core_session,
+            url,
+            method="GET",
+            timeout=self.settings.connection_timeout).json()
 
-        self.log.info('status=success, action=get_filter_info_v3, msg="Got info for filter {}", '
-                      'hostname="{}"'.format(filter_id, self.__core_hostname))
+        self.log.info(f'status=success, action=get_filter_info_v3, msg="Got info for filter {filter_id!r}", '
+                      f'hostname={self.__core_hostname!r}')
 
-        return {"name": filters.get("name"),
-                "folder_id": filters.get("folderId"),
-                "removed": filters.get("isRemoved"),
-                "source": filters.get("source"),
-                "pdqlQuery": filters.get("pdqlQuery")
-                }
+        return {
+            "name": filters.get("name"),
+            "folder_id": filters.get("folderId"),
+            "removed": filters.get("isRemoved"),
+            "source": filters.get("source"),
+            "pdqlQuery": filters.get("pdqlQuery"),
+        }
 
     def close(self):
         if self.__core_session is not None:
--- tests/unit_filters.py
+++ tests/unit_filters.py
@@ -1,20 +1,21 @@
+import json
 import unittest
 
 from mpsiemlib.common import *
 from mpsiemlib.modules import MPSIEMWorker
-from tests.settings import creds, settings
+from settings import settings, creds_pat
 
 
 class FiltersTestCase(unittest.TestCase):
     __mpsiemworker = None
     __module = None
-    __creds_ldap = creds
+    __creds = creds_pat
     __settings = settings
 
     @classmethod
     def setUpClass(cls) -> None:
-        cls.__mpsiemworker = MPSIEMWorker(cls.__creds_ldap, cls.__settings)
-        cls.__module = cls.__mpsiemworker.get_module(ModuleNames.FILTERS)
+        cls.__mpsiemworker = MPSIEMWorker(cls.__creds, cls.__settings)
+        cls.__module = cls.__mpsiemworker.get_module(ModuleNames.FILTERS)   # noqa
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -23,16 +24,22 @@ class FiltersTestCase(unittest.TestCase):
     def test_get_folders_list(self):
         folders = self.__module.get_folders_list()
 
+        print(json.dumps(folders, indent=4, ensure_ascii=False))
+
         self.assertGreater(len(folders), 0)
 
     def test_get_filters_list(self):
         filters = self.__module.get_filters_list()
+
+        print(json.dumps(filters, indent=4, ensure_ascii=False))
 
         self.assertGreater(len(filters), 0)
 
     def test_get_filter_info(self):
         filter_id = next(iter(self.__module.get_filters_list()))
         filter_info = self.__module.get_filter_info(filter_id)
+
+        print(json.dumps(filter_info, indent=4, ensure_ascii=False))
 
         self.assertGreater(len(filter_info), 0)
 
